5c0e315ad9df25eda40867522c05ac39e8045a5f
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-01-08 23:51:04 +0545

    LoginLayoutPage
    Login is rendered

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-using Coursework1.Data;
+using MudBlazor.Services;
 
 namespace Coursework1;
 
@@ -16,13 +16,14 @@ public static class MauiProgram
 			});
 
 		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddMudServices();
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
 
-		builder.Services.AddSingleton<WeatherForecastService>();
+		
 
 		return builder.Build();
 	}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+using Coursework1.Model;
+using Coursework1.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace Coursework1.Pages
+{
+    public partial class Index
+    {
+        private List<User> users;
+        private ICsvHelper csvHelper;
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            csvHelper = new MyCsvHelper();
+            users = csvHelper.ReadUsersFromCsv();
+            if (users != null)
+            {
+                // Successful login, navigate to another page
+                NavigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                // Failed login, display error or handle accordingly
+                Console.WriteLine("Invalid username or password");
+            }
+        }
+
+       
+    }
+}
